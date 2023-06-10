a46a593c0a1c9428b3ba0a9f59217dde9c000151
simostor <simonstoraker@gmail.com>
2023-06-10 21:48:19 +0200

    Initial test OK
    This program tests the functionality of the module. It spins a motor at full speed. The output voltage of the L298N motor controller was measured to be almost 11V, which hopefully should provide enough power to run the pump at a satisfactory speed.  The next step is to plan the general structure of the program, and to make my own motor controller library.

--- Codebase/src/main.cpp
+++ Codebase/src/main.cpp
@@ -1,18 +1,14 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int PWM_PIN = 11;
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+Serial.begin(9600);
+pinMode(PWM_PIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+int pwm = 255;
+analogWrite(PWM_PIN, pwm);
 }
